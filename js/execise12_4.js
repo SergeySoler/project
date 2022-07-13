0aeba686e1e78c7085ce951d448e174d884f9293
SergeySoler <cergey.9135@gmail.com>
2022-07-13 10:39:38 +0300

    up execise 12_4

--- js/execise12_4.js
+++ js/execise12_4.js
@@ -1,2 +1,22 @@
 "use strict";
 
+const baseCurrencies = ['USD', 'EUR'];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr) {
+    if (arr.length === 0) {
+        return 'Нет доступных валют';
+    }
+    arr.forEach(function (item, i) {
+        if (item === missingCurr) {
+            arr.splice(i, 1);
+        }
+    });
+    let str = 'Доступные валюты:\n';
+    arr.forEach(function (item, i) {
+        str += `${item}\n`;
+    });
+    return str;
+}
+console.log(availableCurr([...baseCurrencies, ...additionalCurrencies],
+    [...baseCurrencies, ...additionalCurrencies][0]));
