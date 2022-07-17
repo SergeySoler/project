f2fea2d5c5d68fb5948deecc3ed55f60bed825d6
Sergey <cergey.9135@gmail.com>
2022-07-18 00:16:08 +0300

    add_new_project_movies

--- project_mDB/project/js/script.js
+++ project_mDB/project/js/script.js
@@ -12,35 +12,76 @@
 
 5) Добавить нумерацию выведенных фильмов */
 
+/* Задания на урок:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
 'use strict';
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
-const advertising = document.querySelectorAll('.promo__adv img'),
-      poster = document.querySelector('.promo__bg'),  
-      genre = poster.querySelectorAll('.promo__genre'),
-      moviesList = document.querySelector('.promo__interactive-list');
-      
-advertising.forEach((item)=>{
-item.remove();
-});
-genre[0].textContent = 'ДРАМА';
-poster.style.backgroundImage = 'url("img/bg.jpg")';
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    const advertising = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelectorAll('.promo__genre'),
+        moviesList = document.querySelector('.promo__interactive-list'),
+        inputFilm = document.querySelector('.promo__interactive .add input[type="text"]'),
+        buttonAddFilms = document.querySelector('.promo__interactive .add button');
 
-moviesList.innerHTML = "";
-movieDB.movies.sort();
-movieDB.movies.forEach((item, i) => {
-    moviesList.innerHTML += `
+    advertising.forEach((item) => {
+        item.remove();
+    });
+    genre[0].textContent = 'ДРАМА';
+    poster.style.backgroundImage = 'url("img/bg.jpg")';
+
+    moviesList.innerHTML = "";
+    movieDB.movies.sort();
+
+    movieDB.movies.forEach((item, i) => {
+
+        moviesList.innerHTML += `
+        <li class="promo__interactive-item">${i+1} ${item}
+            <div class="delete"></div>
+        </li>
+        `;
+
+    });
+
+
+    buttonAddFilms.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(inputFilm.value);
+        movieDB.movies.push(inputFilm.value);
+
+        movieDB.movies.forEach((item, i) => {
+            if (i + 1 === movieDB.movies.length) {
+                moviesList.innerHTML += `
     <li class="promo__interactive-item">${i+1} ${item}
         <div class="delete"></div>
     </li>
     `;
-});
+            }
+        });
 
+    });
+});
